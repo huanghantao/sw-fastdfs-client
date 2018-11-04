1f5335cf25036faf8898314e314dc629b3247a94
codinghuang <2812240764@qq.com>
2018-11-04 12:24:26 +0800

    Fix: The default parameter of the Client

--- src/Client.php
+++ src/Client.php
@@ -4,17 +4,20 @@ namespace Codinghuang\SwFastDFSClient;
 
 class Client
 {
+    const DEFAULT_HOST = "127.0.0.1";
+    const DEFAULT_PORT = 22122;
+
     private $tracker;
     private $config;
 
-    public function __construct(array $config)
+    public function __construct(array $config = null)
     {
         $this->config = $config;
-        if (!isset($config['host'])) {
-            $this->config['host'] = "127.0.0.1";
+        if (!isset($config) || !isset($config['host'])) {
+            $this->config['host'] = Client::DEFAULT_HOST;
         }
-        if (!isset($config['port'])) {
-            $this->config['port'] = 22122;
+        if (!isset($config) || !isset($config['port'])) {
+            $this->config['port'] = Client::DEFAULT_PORT;
         }
         $this->tracker = new Tracker($this->config['host'], $this->config['port']);
     }
--- src/test.php
+++ src/test.php
@@ -5,11 +5,11 @@ use Codinghuang\SwFastDFSClient\Error;
 
 require '../vendor/autoload.php';
 
-$config = [
-    'host' => '127.0.0.1',
-    'port' => 2212
-];
-$client = new Client($config);
+// $config = [
+//     'host' => '127.0.0.1',
+//     'port' => 22122
+// ];
+$client = new Client();
 if (!$client->connect()) {
     print_r(Error::$errMsg . PHP_EOL);
 }
